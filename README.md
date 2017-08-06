51f8c31c78df492bbace3d888c6906746b7a324e
Dan Garnys <dgarnys@gmail.com>
2017-08-06 14:20:22 +0100

    Setting up

---
+++ README.md
@@ -0,0 +1,1 @@
+# Crypto-Unit-Cost-Averager
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {figlet} from './figlet';
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
